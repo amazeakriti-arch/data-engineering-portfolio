6b29cac8761456c803e0c8cd30c9124acfec5e1d
amazeakriti-arch <amaze.akriti@gmail.com>
2025-12-18 14:49:44 +0530

    Add files via upload

---
+++ week3-oracle-etl/extract.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from config import ENGINE
+
+def extract_data():
+    query = "SELECT * FROM fin_rep"
+    df = pd.read_sql(query, ENGINE)
+    return df
+
+if __name__ == "__main__":
+    df = extract_data()
+    print("Rows extracted:", len(df))
