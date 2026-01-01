03fead5938fdee37a886cc7d70535045fcc0b18f
amazeakriti-arch <amaze.akriti@gmail.com>
2026-01-01 18:00:30 +0530

    Update config.py

--- week3-oracle-etl/config.py
+++ week3-oracle-etl/config.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
-USERNAME = "SYSTEM"
+USERNAME = "system"
 PASSWORD = quote_plus("system")
 HOST = "localhost"
 PORT = 1521
-SID = "orcle"
+SERVICE_NAME = "orcl"
 
 ENGINE = create_engine(
-    f"oracle+oracledb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{SID}"
+    f"oracle+oracledb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/?service_name={SERVICE_NAME}"
 )
